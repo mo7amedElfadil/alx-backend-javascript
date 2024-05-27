b28b3205a1347a71cc2cf3fd5a26abff9b0ccaa0
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-05-27 16:17:43 +0300

    testing space before parant

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -4,16 +4,16 @@
 // let is used to declare variables that can be reassigned
 // const and let are block-scoped
 
-export function taskFirst () {
+export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-export function getLast () {
+export function getLast() {
   return ' is okay';
 }
 
-export function taskNext () {
+export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
 
