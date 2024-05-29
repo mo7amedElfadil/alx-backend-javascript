fe02745ee84402ea071cbae5a378a71c9161613c
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-05-29 14:17:06 +0300

    using else

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -7,6 +7,7 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw new Error('Cannot divide by 0');
+  } else {
+    return numerator / denominator;
   }
-  return numerator / denominator;
 }
