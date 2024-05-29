9085a32727c473ff5b0324c36bae75aa96d526c1
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-05-29 14:07:52 +0300

    return promise t3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,7 +6,7 @@ import { uploadPhoto, createUser } from './util';
  * @returns {undefined}
  */
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(values[0].body, values[1].firstName, values[1].lastName);
     })
