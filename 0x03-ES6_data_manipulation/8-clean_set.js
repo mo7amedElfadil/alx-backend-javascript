0474dd8e7047010b330e9fb942b8a0229b9eef16
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-06-05 01:56:19 +0300

    removed empty str cond

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,9 +6,6 @@
  * @return {string}
  */
 export default function cleanSet(set, startString) {
-  if (startString === '') {
-    return '';
-  }
   return Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
