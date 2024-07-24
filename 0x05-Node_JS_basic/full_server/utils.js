05745aad374330622257a861a5e73050dce2859e
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-07-24 04:19:25 +0300

    import errors

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -3,7 +3,7 @@
 import fs from 'fs';
 
 /**
- * function readDataBase - accepts a file path
+ * function readDatabase - accepts a file path
  * Reads the data asynchronously and returns a promise
  * when file is not accessible, rejects the promise with the error
  * when file is accessible, resolves the promise with the object of arrays of the
@@ -13,7 +13,7 @@ import fs from 'fs';
  * @throws {Error}
  */
 
-function readDataBase(filePath) {
+function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
@@ -44,4 +44,4 @@ function readDataBase(filePath) {
   });
 }
 
-export default readDataBase;
+export default readDatabase;
